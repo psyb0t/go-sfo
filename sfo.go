824ae75544691f0d53e2e8890d631415225d0faa
Ciprian Mandache <mandacheciprian@ymail.com>
2016-10-13 04:06:11 +0300

    Add Create and Writes and Checks - the usual fop reqs

--- sfo.go
+++ sfo.go
@@ -4,6 +4,8 @@ package sfo
 import (
     "os"
     "io"
+    "bytes"
+    "path/filepath"
 )
 
 type File struct {
@@ -12,6 +14,16 @@ type File struct {
 }
 
 func ReadFile(file_path *string) (*File, error) {
+    exists, err := PathExists(file_path)
+
+    if err != nil {
+        return nil, err
+    }
+
+    if !exists {
+        return &File{}, nil
+    }
+
     f, err := os.OpenFile(*file_path, os.O_RDONLY, 0644)
     defer f.Close()
 
@@ -19,22 +31,135 @@ func ReadFile(file_path *string) (*File, error) {
         return nil, err
     }
 
-    bytes := make([]byte, 1024)
+    file_bytes := make([]byte, 1024)
     for {
-        part, err := f.Read(bytes)
+        _, err = f.Read(file_bytes)
 
-        if err != io.EOF {
-            return nil, err
+        if err == io.EOF {
+            break
         }
 
-        if part == 0 {
-            break
+        if err != nil {
+            return nil, err
         }
     }
 
+    file_bytes = bytes.Trim(file_bytes, "\x00")
+
     file := &File{
-        Bytes: bytes,
-        Text: string(bytes),
+        Bytes: file_bytes,
+        Text: string(file_bytes),
+    }
+
+    return file, nil
+}
+
+func CreateFile(file_path *string) error {
+    exists, err := PathExists(file_path)
+
+    if err != nil {
+        return err
+    }
+
+    if exists {
+        return nil
+    }
+
+    dir := filepath.Dir(*file_path)
+
+    exists, err = PathExists(&dir)
+
+    if err != nil {
+        return err
+    }
+
+    if !exists {
+        err = os.MkdirAll(dir, 0644)
+
+        if err != nil {
+            return err
+        }
+    }
+
+    file, err := os.Create(*file_path)
+    defer file.Close()
+
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func WriteStringToFile(file_path *string, text *string) error {
+    file, err := ReadyFile(file_path)
+
+    if err != nil {
+        return err
+    }
+
+    _, err = file.WriteString(*text)
+
+    if err != nil {
+        return err
+    }
+
+    err = file.Sync()
+
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func WriteBytesToFile(file_path *string, bytes *[]byte) error {
+    file, err := ReadyFile(file_path)
+
+    if err != nil {
+        return err
+    }
+
+    _, err = file.Write(*bytes)
+
+    if err != nil {
+        return err
+    }
+
+    err = file.Sync()
+
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func PathExists(path *string) (bool, error) {
+    _, err := os.Stat(*path)
+
+    if os.IsNotExist(err) {
+        return false, nil
+    }
+
+    if err != nil {
+        return false, err
+    }
+
+    return true, nil
+}
+
+func ReadyFile(file_path *string) (*os.File, error) {
+    err := CreateFile(file_path)
+
+    if err != nil {
+        return nil, err
+    }
+
+    file, err := os.OpenFile(*file_path, os.O_WRONLY, 0644)
+
+    if err != nil {
+        return nil, err
     }
 
     return file, nil
